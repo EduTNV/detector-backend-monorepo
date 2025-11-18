062f61a89eed91192e0e463c3b3620a5ad9b4801
Eduardo Travassos <169100607+EduTNV@users.noreply.github.com>
2025-11-17 22:04:50 -0300

    Gateway

---
+++ servico-gateway/src/test/java/com/projetoA3/servico_gateway/ServicoGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetoA3.servico_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicoGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
